b396ce835d81259705df9b27cbad044cabc4ee78
Charlotte <charlotte.bourg@gmail.com>
2023-03-30 17:38:51 -0700

    updated site

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 from pprint import pformat
 import os
 import requests
+from random import choice
 
 
 app = Flask(__name__)
@@ -42,7 +43,11 @@ def find_afterparties():
 
     url = 'https://app.ticketmaster.com/discovery/v2/events'
     payload = {'apikey': API_KEY}
-
+    payload['keyword'] = keyword
+    payload['postalCode'] = postalcode
+    payload['radius'] = radius
+    payload['unit'] = unit
+    payload['sort'] = sort
     # TODO: Make a request to the Event Search endpoint to search for events
     #
     # - Use form data from the user to populate any search parameters
@@ -53,11 +58,11 @@ def find_afterparties():
     #
     # - Replace the empty list in `events` with the list of events from your
     #   search results
-
-    data = {'Test': ['This is just some test data'],
-            'page': {'totalElements': 1}}
-    events = []
-
+    res = requests.get(url, params = payload)
+    data = res.json()
+    if '_embedded' in data: 
+        events = data['_embedded']['events']
+    else: events = []
     return render_template('search-results.html',
                            pformat=pformat,
                            data=data,
@@ -72,10 +77,23 @@ def find_afterparties():
 @app.route('/event/<id>')
 def get_event_details(id):
     """View the details of an event."""
-
+    url = f'https://app.ticketmaster.com/discovery/v2/events/{id}'
+    payload = {'apikey': API_KEY}
+    res = requests.get(url, params = payload)
+    data = res.json()
+    img_url = ""
+    venues = []
+    if 'images' in data:
+        print(len(data['images']))
+        print(type(data['images']))
+        img_url = choice(data['images'])['url']
+    if '_embedded' in data: 
+        venues = data['_embedded']['venues']
+    #event_description = "placeholder"
+    #event_description = data['description']
     # TODO: Finish implementing this view function
 
-    return render_template('event-details.html')
+    return render_template('event-details.html',data=data,img_url=img_url, venues = venues)
 
 
 if __name__ == '__main__':
